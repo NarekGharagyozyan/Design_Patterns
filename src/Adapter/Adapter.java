f6fb9fe0c06daad7b1327a67e55191d5f4898323
NarekGharagyozyan <narekgharagyozyann@gmail.com>
2023-06-02 19:03:27 +0400

    some changes

--- src/Adapter/Adapter.java
+++ src/Adapter/Adapter.java
@@ -1,11 +1,8 @@
 package Adapter;
 
 public class Adapter implements Sensor{
-    private FahrenheitSensor fahrenheitSensor;
+    private FahrenheitSensor fahrenheitSensor = new FahrenheitSensor();
 
-    public Adapter(FahrenheitSensor fahrenheitSensor){
-        this.fahrenheitSensor = fahrenheitSensor;
-    }
     @Override
     public double getTemperature() {
         return (fahrenheitSensor.getFahrenheitTemp() - 32) * 5.0 / 9.0;
--- src/Adapter/Main.java
+++ src/Adapter/Main.java
@@ -2,8 +2,7 @@ package Adapter;
 
 public class Main {
     public static void main(String[] args) {
-        Sensor sensor = new Adapter(new FahrenheitSensor());
-
+        Sensor sensor = new Adapter();
         System.out.println(sensor.getTemperature());
     }
 }
